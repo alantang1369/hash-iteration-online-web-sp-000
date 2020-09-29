4961a18abe224a873500f5b8b89ef7bfb287c938
Alan Tang <giabao_tang@yahoo.com>
2020-09-29 00:41:40 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -6,12 +6,12 @@
 
 def happy_birthday(birthday_kids)
   # add your code snippet here!
-    birthday_kids.each do |kids_name, age|
+   birthday_kids.each do |kids_name, age|
     
     puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
     end
 end
-end
+end 
 
 def age_appropriate_birthday(birthday_kids)
   
